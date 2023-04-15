8d8a68186783ffb3983c3c22354f9904a10e391e
Tom Easterbrook <tomeasterbrook98@gmail.com>
2023-04-15 11:29:46 +0100

    Add required params

--- OtpInput.js
+++ OtpInput.js
@@ -1,12 +1,21 @@
 import {StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
 import React, {useEffect, useRef, useState} from "react";
 
-const OtpInput = ({value, onValueChanged,onAutofill,onNewCodeRequested}) => {
-    const [code,setCode] = useState(['','','','','',''])
+const setInitialCode = (numberOfDigits,value) => {
+    const code = []
+    for (let i = 0; i < numberOfDigits; i++) {
+        code[i] = value[i] || ""
+    }
+    return code
+}
+const OtpInput = ({value = [],numberOfDigits = 4, onValueChanged=()=>{},onAutofill=()=>{},onNewCodeRequested = ()=>{},containerStyle = {},headerTextStyle = {},digitInputStyle = {}, codeResendEnabledStyle = {},codeResendDisabledStyle: codeResendDisabledStyle = {}, hideCode = false, headerText = "Enter your One Time Passcode"}) => {
+    const [code,setCode] = useState(setInitialCode(numberOfDigits,value))
     const [newCodeTimer,setNewCodeTimer] = useState(30)
     const refs = useRef([])
 
     let newCodeButtonEnabled = newCodeTimer === 0;
+
+
 
     useEffect(()=>{
         onValueChanged(code)
@@ -20,7 +29,7 @@ const OtpInput = ({value, onValueChanged,onAutofill,onNewCodeRequested}) => {
         }
     },[newCodeTimer])
     const changeDigit = (index,digit) => {
-        if (digit.length === 6){
+        if (digit.length === numberOfDigits){
             let autofillCode = digit.split("")
             setCode(autofillCode)
             onValueChanged(autofillCode)
@@ -40,17 +49,58 @@ const OtpInput = ({value, onValueChanged,onAutofill,onNewCodeRequested}) => {
             if (code[index] === ''){
                 refs.current[index-1].focus()
             }
-        }else if (index<5 && event.key !== 'Backspace'){
+        }else if (index<numberOfDigits-1 && event.key !== 'Backspace'){
             refs.current[index+1].focus()
 
         }
       
     }
+    const styles = StyleSheet.create({
+        container:{
+            padding:10,
+            flexDirection:'row',
+            justifyContent:'center',
+            ...containerStyle
+        },
+        title: {
+            fontWeight:'bold',
+            textAlign:'center',
+            paddingTop:10,
+            paddingBottom:10,
+            fontSize:20,
+            ...headerTextStyle
+        },
+        resendCodeButtonEnabled: {
+            color:'blue',
+            textAlign:'right',
+            paddingBottom:5,
+            ...codeResendEnabledStyle
+        },
+        resendCodeButtonDisabled: {
+            color:'grey',
+            textAlign:'right',
+            paddingBottom:5,
+            ...codeResendDisabledStyle
+        },
+        numberBox:{
+            width:'10%',
+            fontSize:30,
+            textAlign:'center',
+            fontWeight:'500',
+            borderStyle:'solid',
+            borderColor:'black',
+            borderRadius:5,
+            borderWidth:2,
+            marginLeft:10,
+            marginRight:10,
+            ...digitInputStyle
+        }
+    })
   // @ts-ignore
     return(
         <View>
             <View>
-                <Text variant={"titleLarge"} style={styles.title}> Enter your One Time Passcode</Text>
+                <Text style={styles.title}>{headerText}</Text>
             </View>
             <View style={styles.container}>
                 {code.map((value,index) =>(
@@ -58,7 +108,8 @@ const OtpInput = ({value, onValueChanged,onAutofill,onNewCodeRequested}) => {
                                onKeyPress={({nativeEvent})=>onKeyPress(index,nativeEvent)}
                                onChangeText={(text)=>changeDigit(index,text)}
                                style={styles.numberBox} value={value} keyboardType={"number-pad"}
-                               maxLength={index===0 ? 6 : 1}
+                               maxLength={index===0 ? numberOfDigits : 1}
+                               secureTextEntry={hideCode}
                                ref={ref => !refs.current.includes(ref) && refs.current.push(ref)}
                     />
 
@@ -82,40 +133,7 @@ const OtpInput = ({value, onValueChanged,onAutofill,onNewCodeRequested}) => {
 
   )
 
+
 }
-const styles = StyleSheet.create({
-    container:{
-        padding:10,
-        flexDirection:'row',
-        justifyContent:'center'
-    },
-    title: {
-        fontWeight:'bold',
-        textAlign:'center',
-        paddingTop:10,
-        paddingBottom:10
-    },
-    resendCodeButtonEnabled: {
-        color:'blue',
-        textAlign:'right',
-        paddingBottom:5
-    },
-    resendCodeButtonDisabled: {
-        color:'grey',
-        textAlign:'right',
-        paddingBottom:5
-    },
-    numberBox:{
-        width:'10%',
-        fontSize:30,
-        textAlign:'center',
-        fontWeight:'500',
-        borderStyle:'solid',
-        borderColor:'black',
-        borderRadius:5,
-        borderWidth:2,
-        marginLeft:10,
-        marginRight:10
-    }
-})
+
 export default OtpInput
